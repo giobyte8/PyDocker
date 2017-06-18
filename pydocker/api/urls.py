40b0c29f0f47352a6861fb0f25c3643121e8710c
Giovanni Aguirre <giovanni.fi05@gmail.com>
2017-06-17 22:06:48 -0500

    :rocket: Improving endpoints names

--- pydocker/api/urls.py
+++ pydocker/api/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/containers/start', views.start_container, name='containers_start'),
-    url(r'^/containers/stop', views.stop_container, name='containers_stop'),
+    url(r'^/container/start', views.start_container, name='container_start'),
+    url(r'^/container/stop', views.stop_container, name='container_stop'),
 ]
